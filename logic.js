fa1c6e64b2e5156054f3ba63645b93b659627b9e
FabioGavinelli <fabiogavinelli@gmail.com>
2026-02-06 11:16:49 +0100

    Add grid to init phase
    The initialization function calls a buildGrid function that draws a
    16x16 grid in the page.
    Right now cells dimension is fixed to 20px x 20px.
    Cells are set to box-sizing=border-box to evalauate easily the grid size

---
+++ logic.js
@@ -0,0 +1,32 @@
+function initialization() {
+    buildGrid(16, 16);
+}
+
+function buildGrid(width, height) {
+    const grid = document.createElement("div");
+    grid.id = "grid";
+    grid.style.display = "flex";
+    grid.style.flexWrap = "wrap";
+
+    for(let i = 0; i < width; i++)
+    {
+        for(let j = 0; j < height; j++)
+        {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.style.boxSizing = "border-box";
+            cell.style.border = "solid 1px black";
+            cell.style.width = "20px";
+            cell.style.height = "20px";
+            cell.style.flexBasis = "1";
+            grid.appendChild(cell);
+        }
+    }
+
+    grid.style.width = `${20*width}px`;
+    grid.style.height = `${20*height}px`;
+
+    document.body.appendChild(grid);
+}
+
+initialization();
