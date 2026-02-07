ede16cba1cf2c6e09ff174b3afd50d0eba85089d
Fabio Gavinelli <fabiogavinelli@gmail.com>
2026-02-07 10:24:33 +0100

    Add reset function to rebuild the grid

--- logic.js
+++ logic.js
@@ -1,32 +1,29 @@
-function initialization() {   
-    buildGrid(16, 16);
-}
 
 function buildGrid(width, height) {
     const grid = document.querySelector("#grid");
     console.log(grid.offsetWidth);
-
+    
     for(let i = 0; i < width; i++)
-    {
-        for(let j = 0; j < height; j++)
         {
-            const cell = buildCell(grid.offsetWidth, grid.offsetHeight);
-            grid.appendChild(cell);
+            for(let j = 0; j < height; j++)
+                {
+                    const cell = buildCell(grid.offsetWidth, grid.offsetHeight, width, height);
+                    grid.appendChild(cell);
+                    
+                    cell.addEventListener("mouseover", changeColorOnMouseOver);
+                }
+            }
             
-            cell.addEventListener("mouseover", changeColorOnMouseOver);
+            document.body.appendChild(grid);
         }
-    }
-
-    document.body.appendChild(grid);
-}
-
-function buildCell(gridWidth, gridHeight) {
+        
+function buildCell(gridWidth, gridHeight, rowElements, colElements) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.style.boxSizing = "border-box";
     cell.style.border = "solid 1px black";
-    cell.style.width = `${gridWidth / 16}px`;
-    cell.style.height = `${gridHeight / 16}px`;
+    cell.style.width = `${gridWidth / rowElements}px`;
+    cell.style.height = `${gridHeight / colElements}px`;
     cell.style.flexBasis = "1";
     return cell;
 }
@@ -35,4 +32,31 @@ function changeColorOnMouseOver(event) {
     event.target.classList.add("hovered");
 }
 
-initialization();
+function setUpResetButton() {
+    const resetBtn = document.querySelector("#rstBtn");
+
+    resetBtn.addEventListener("click", () => {
+
+        let dim = 0;
+
+        do {
+            dim = prompt("Insert the new grid dimension (max 100)");
+        } while (dim > 100)
+        
+
+        resetGrid(dim, dim);
+    });
+}
+
+function resetGrid(width, height) {
+    const grid = document.querySelector("#grid");
+    
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+
+    buildGrid(width, height);
+}
+
+buildGrid(16, 16);
+setUpResetButton();
